d39b60199256ca026f5051e0e36205339e6d9061
kavinraju <rajukavin25@gmail.com>
2021-01-30 07:32:50 +0530

    Linting fix

--- Algorithmic-Toolbox/Practice-3-Greedy-Algorithm/groupingOfChildren.cpp
+++ Algorithmic-Toolbox/Practice-3-Greedy-Algorithm/groupingOfChildren.cpp
@@ -1,19 +1,21 @@
-#include<iostream>
-#include<vector>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-
 // This function returns the number of groups, minimum number of groups
-int pointsCoveredSorted(vector<float> x, int n){
+int pointsCoveredSorted(vector<float> x, int n)
+{
 	//vector<vector<int>> R;
 	int R = 0;
 	int i = 0;
-	while(i<n){
+	while (i < n)
+	{
 		int l = x.at(i), r = x.at(i) + 1;
 		R += 1;
 		i += 1;
-		while(i < n && x.at(i) <= r){
+		while (i < n && x.at(i) <= r)
+		{
 			i += 1;
 		}
 	}
@@ -21,14 +23,17 @@ int pointsCoveredSorted(vector<float> x, int n){
 }
 
 // This function returns the number of groups
-vector<vector<float> > getGroupings(vector<float> x, int n){
-	vector<vector<float> > R;
-	int i = 0, j =0;
-	while(i<n){
+vector<vector<float>> getGroupings(vector<float> x, int n)
+{
+	vector<vector<float>> R;
+	int i = 0, j = 0;
+	while (i < n)
+	{
 		int l = x.at(i), r = x.at(i) + 1;
 		R.push_back(vector<float>({x.at(i)}));
 		i += 1;
-		while(i < n && x.at(i) <= r){
+		while (i < n && x.at(i) <= r)
+		{
 			R.at(j).push_back(x.at(i));
 			i += 1;
 		}
@@ -37,33 +42,35 @@ vector<vector<float> > getGroupings(vector<float> x, int n){
 	return R;
 }
 
-int main(){
+int main()
+{
 	vector<float> childrenAges;
-	cout<<"Number of children: ";
+	cout << "Number of children: ";
 	int t;
-	cin>>t;
-	cout<<"Enter the ages of children: "<<endl;
-	for(int i=0; i<t;i++){
+	cin >> t;
+	cout << "Enter the ages of children: " << endl;
+	for (int i = 0; i < t; i++)
+	{
 		float x;
-		cin>>x;
+		cin >> x;
 		childrenAges.push_back(x);
 	}
-	
+
 	sort(childrenAges.begin(), childrenAges.end());
-	cout<<"Number of groups(minimum number of groups): "<<pointsCoveredSorted(childrenAges, childrenAges.size())<<endl;
-	
-	vector<vector<float> > groupList = getGroupings(childrenAges, childrenAges.size());
+	cout << "Number of groups(minimum number of groups): " << pointsCoveredSorted(childrenAges, childrenAges.size()) << endl;
+
+	vector<vector<float>> groupList = getGroupings(childrenAges, childrenAges.size());
 	/*for(int i=0; i<groupList.size();i++){
 		copy(groupList.at(i).begin(), groupList.at(i).end(), ostream_iterator<vector<float> >(cout, "\n"));
 	}*/
-	
-	for(int j=0; j<groupList.size();j++){
-		cout<<"Group "<<j+1<<endl;
+
+	for (int j = 0; j < groupList.size(); j++)
+	{
+		cout << "Group " << j + 1 << endl;
 		for (std::vector<float>::const_iterator i = groupList.at(j).begin(); i != groupList.at(j).end(); ++i)
-    		std::cout << *i << ' ';
-    	cout<<endl;
-    	}
-    	
-return 0;
+			std::cout << *i << ' ';
+		cout << endl;
 	}
 
+	return 0;
+}
