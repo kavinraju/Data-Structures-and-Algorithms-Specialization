becd52e5010151286040a258ece3cb91df40b210
kavinraju <rajukavin25@gmail.com>
2021-02-26 12:50:21 +0530

    Added Length_of_longest_odd_even_subarray

---
+++ Arrays/Length_of_longest_odd_even_subarray.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// https://www.geeksforgeeks.org/length-of-the-longest-alternating-even-odd-subarray/
+
+int lengthOfLongestOddEvenSubarray(int arr[], int n)
+{
+    int counter = 1, max_length = 0;
+    for (int i = 0; i < n - 1; i++)
+    {
+        if ((arr[i] + arr[i + 1]) % 2 != 0){
+        	counter++;
+			max_length = counter;
+		}
+        else
+            counter = 1;
+    }
+    return max_length;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    int longest_length = lengthOfLongestOddEvenSubarray(arr, n);
+    cout << "Length Of Longest Odd Even Subarray = " << longest_length << endl;
+
+    return 0;
+}
