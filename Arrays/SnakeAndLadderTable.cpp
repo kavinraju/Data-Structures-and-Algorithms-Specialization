c4717dd55876d1b39337f3097d0b83996e9d315e
kavinraju <rajukavin25@gmail.com>
2021-05-29 12:51:27 +0530

    Added the SnakeAndLadderTable program logic for both the even and odd with different n m values.

--- Arrays/SnakeAndLadderTable.cpp
+++ Arrays/SnakeAndLadderTable.cpp
@@ -30,34 +30,40 @@ Output To be printed
 int main()
 {
     int n, m;
+    cout<<"Enter n & m values: ";
     cin >> n >> m;
     //n = 3, m =4;
     int N = n * n;
     int i = 0, j =0;
     bool isEven = n % 2 == 0;
 
+    // Update the value of j based on the values of n.
     if(isEven) 
         j = 0; 
     else 
         j = n;
 
+    cout<<"Symmetrical pattern: n = "<<n<<endl;
+    // This while loop works for the Symmetrical pattern that is N = n * n
     while(i < n){
         if(j == 0){
-            //j = 0, 1, 2, 3, 4
-            //16, 15, 14, 13
+            /*the below Example is for n = 4 and not considering the m value
+            j = 0, 1, 2, 3, 4
+            16, 15, 14, 13
+            */
             while(j <= n-1){
                 cout<<N--<<"\t";
-                //N -= (j+1);
                 j++;
             }
         }else if( j == n ){
-            // j = 4, 3, 2, 1, 0
-            // jj = 3, 2, 1, 0 | 2, 1, 0 | 1, 0 | 0
+            /* the below Example is for n = 4 and not considering the m value
+                j = 4, 3, 2, 1, 0
+                 jj = 3, 2, 1, 0 | 2, 1, 0 | 1, 0 | 0
+            */
             N -= (n-1);
             j =0;
             while(j <= n-1){
                 cout<<N++<<"\t";
-                //N -= (j+1);
                 j++;
             }
             j = 0;
@@ -67,5 +73,43 @@ int main()
         i++;
         cout<<endl;
     }
+
+    cout<<"Asymmetrical pattern: n = "<<n<<" & m = "<<m<<endl;
+
+    // Update the values:
+    i = 0, j = 0, N = n * m;
+    isEven = n % 2 == 0;
+
+    // Update the value of j based on the values of n.
+    if(isEven) 
+        j = 0; 
+    else 
+        j = m;
+
+    // This while loop works for the Asymmetrical pattern that is N = n * n
+    while(i < n){
+        if(j == 0){
+
+            while(j <= m-1){
+                cout<<N--<<"\t";
+                j++;
+            }
+
+        }else if( j == m ){
+
+            N -= (m-1);
+            j =0;
+            while(j <= m-1){
+                cout<<N++<<"\t";
+                j++;
+            }
+            j = 0;
+            N -= (m+1);
+        }
+
+        i++;
+        cout<<endl;
+    }
+
     return 0;
 }
